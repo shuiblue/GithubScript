9d2ef496720e56c9e4934bcd1da6d10386945e1d
Shurui Zhou <shuruiz@Shuruis-MacBook-Pro.local>
2017-10-19 11:39:49 -0400

    start implementing track commit history analyzer

--- src/GetActiveForks.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by shuruiz on 10/8/17.
- */
-public class GetActiveForks {
-}
---
+++ src/TrackCommitHistory.java
@@ -0,0 +1,11 @@
+/**
+ * Created by shuruiz on 10/8/17.
+ */
+public class TrackCommitHistory {
+
+    public void getForkList(String repo_url){
+
+    }
+
+
+}
