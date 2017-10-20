8d20ca894a7f2889ff408f648244e9baf2890708
Shurui Zhou <shuruiz@Shuruis-MacBook-Pro.local>
2017-10-20 13:54:54 -0400

    change dir path

---
+++ src/IO_Process.java
@@ -0,0 +1,74 @@
+import java.io.*;
+
+/**
+ * Created by shuruiz on 10/19/17.
+ */
+public class IO_Process {
+        static String current_OS = System.getProperty("os.name").toLowerCase();
+
+        public void writeTofile(String content, String filepath) {
+
+
+            try {
+                File file = new File(filepath);
+                // if file doesnt exists, then create it
+                if (!file.exists()) {
+                    file.getParentFile().mkdir();
+                    file.createNewFile();
+                }
+                FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(content);
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void rewriteFile(String content, String filepath) {
+
+            try {
+                File file = new File(filepath);
+                // if file doesnt exists, then create it
+                if (!file.exists()) {
+                    file.getParentFile().mkdirs();
+                    file.createNewFile();
+                }
+                FileWriter fw = new FileWriter(file.getAbsoluteFile(), false);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(content);
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+    /**
+     * this function read the content of the file from filePath, and ready for comparing
+     *
+     * @param filePath file path
+     * @return content of the file
+     * @throws IOException e
+     */
+    public String readResult(String filePath) throws IOException {
+        BufferedReader result_br = new BufferedReader(new FileReader(filePath));
+        String result = "";
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = result_br.readLine();
+
+            while (line != null) {
+
+                sb.append(line);
+                sb.append(System.lineSeparator());
+
+                line = result_br.readLine();
+            }
+            result = sb.toString();
+        } finally {
+            result_br.close();
+        }
+        return result;
+    }
+}
--- src/TrackCommitHistory.java
+++ src/TrackCommitHistory.java
@@ -1,11 +1,36 @@
+import java.io.IOException;
+
 /**
  * Created by shuruiz on 10/8/17.
  */
 public class TrackCommitHistory {
-
+     static String token;
+    /**
+     * This function gets a list of forks of a repository
+     * input: repo_url
+     * output: fork list
+     * @param repo_url  e.g. 'shuiblue/INFOX'
+     */
     public void getForkList(String repo_url){
 
     }
 
+
+    public static void main(String[] args){
+        TrackCommitHistory tch = new TrackCommitHistory();
+        IO_Process io = new IO_Process();
+        String root = "/Users/shuruiz/Work/checkProjectSize/";
+
+        String repo="";
+
+
+        try {
+            token = io.readResult(root + "/token.txt").trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        tch.getForkList(repo);
+    }
 
 }
